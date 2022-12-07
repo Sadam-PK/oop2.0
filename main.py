ea174d4905360f0fefd3aa515bfed959b9f77fbb
sadam <sadamccr@gmail.com>
2022-12-07 13:38:16 +0500

    methods updated..

--- main.py
+++ main.py
@@ -12,9 +12,19 @@ class Item:
         self.price = price
         self.quantity = quantity
 
-    def price_calc(self, x, y):
-        return x * y
+    # def price_calc(self, x, y):
+    #     return x * y
+
+    # # re-write the above fun as follows - self means object is passed as argument, so;
+    def price_calc(self):
+        return self.price * self.quantity
 
 
 item1 = Item('Apple', 10, 2)
-print(item1.name, item1.price_calc(item1.price, item1.quantity))
+item2 = Item('Banana', 5, 3)
+
+print(item1.name, item1.price, item1.quantity)
+print(item2.name, item2.price, item2.quantity)
+
+print(f'Item 1 price = {item1.price_calc()}')
+print(f'Item 2 price = {item2.price_calc()}')
