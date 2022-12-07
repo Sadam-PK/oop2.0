d0ab5b4613da995a6f598d6c2f92cb28038338c0
sadam <sadamccr@gmail.com>
2022-12-07 14:51:35 +0500

    special presentation function

--- main.py
+++ main.py
@@ -8,6 +8,7 @@
 
 class Item:
     pay_rate = 0.8
+    all = []
 
     def __init__(self, name, price, quantity):
         assert price >= 0, f'Price should be more than 0'
@@ -15,6 +16,9 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
 
     # def price_calc(self, x, y):
     #     return x * y
@@ -26,16 +30,30 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    def __repr__(self):
+        return f"Item('{self.name}', '{self.price}', {self.quantity})"
+
 
 item1 = Item('Apple', 10, 2)
-item1.apply_discount()
+# item1.apply_discount()
 
 item2 = Item('Banana', 5, 3)
-item2.pay_rate = 0.7
-item2.apply_discount()
 
-print(item1.name, item1.price, item1.quantity)
-print(item2.name, item2.price, item2.quantity)
+item3 = Item('Grapes', 12, 2)
+# #-------- normal actions of adding values and printing them -------
+# item2.pay_rate = 0.7
+# item2.apply_discount()
+#
+# print(item1.name, item1.price, item1.quantity)
+# print(item2.name, item2.price, item2.quantity)
+#
+# print(f'Item 1 price = {item1.price_calc()}')
+# print(f'Item 2 price = {item2.price_calc()}')
 
-print(f'Item 1 price = {item1.price_calc()}')
-print(f'Item 2 price = {item2.price_calc()}')
+# #-------- printing will ALL -------
+#
+# for i in Item.all:
+#     print(i.name)
+
+# #-------- special presentation ----------
+# print(Item.all)
