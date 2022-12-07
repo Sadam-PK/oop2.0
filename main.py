357f14321055b2842c5851ab92b35649e3a3c97f
sadam <sadamccr@gmail.com>
2022-12-07 13:28:36 +0500

    create a base class and constructor

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-item1 = 1
-item2 = 1.2
-item3 = 'apple'
+# item1 = 1
+# item2 = 1.2
+# item3 = 'apple'
+#
+# print(type(item1))
+# print(type(item2))
+# print(type(item3))
 
-print(type(item1))
-print(type(item2))
-print(type(item3))
+class Item:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def price_calc(self, x, y):
+        return x * y
+
+
+item1 = Item('Apple', 10, 2)
+print(item1.name, item1.price_calc(item1.price, item1.quantity))
